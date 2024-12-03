1f13221c017940b113ed80fc69e6b315e8cb5e52
phanthebao1234 <phanthebao888@gmail.com>
2024-12-03 23:22:40 +0700

    reset password page

--- src/app/password-reset/page.tsx
+++ src/app/password-reset/page.tsx
@@ -1,5 +1,31 @@
+import { PasswordResetForm } from "@/src/components/forms"
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+    title: "Full Auth | Password Reset",
+    description: "Full Auth password reset page"
+}
 export default function Page() {
     return (
-        <h1>Reset Password</h1>
-    )
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+
+                <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                    Reset Password
+                </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <PasswordResetForm />
+
+                <p className="mt-10 text-center text-sm/6 text-gray-500">
+                    If you do not account{' '}
+                    <Link href="/auth/register" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                        Register here
+                    </Link>
+                </p>
+            </div>
+        </div>
+    );
 };
